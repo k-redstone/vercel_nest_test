c231731354b1724f8806265c9a1c0efe6f92c18c
k-redstone <che0618@naver.com>
2025-03-17 17:26:29 +0900

    feat: 포트 및 cors 설정

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe, BadRequestException } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://saebaldong.kr'],
   })
 
   app.useGlobalPipes(
@@ -19,6 +19,6 @@ async function bootstrap() {
     }),
   )
 
-  await app.listen(process.env.PORT ?? 3001)
+  await app.listen(process.env.PORT ?? 8000)
 }
 bootstrap()
