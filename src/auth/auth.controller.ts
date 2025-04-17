610e5e40cea06fa423198460e048b79b8e017f52
k-redstone <che0618@naver.com>
2025-04-17 22:16:39 +0900

    feat: 환경별 cookie설정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import {
   Req,
   UseGuards,
   UnauthorizedException,
+  Get,
 } from '@nestjs/common'
 import { AuthService } from '@src/auth/auth.service'
 
@@ -27,20 +28,31 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ) {
     const tokenData = await this.authService.login(loginDto)
+    const isProd = process.env.NODE_ENV === 'production'
 
     res.cookie('accessToken', tokenData.accessToken, {
       httpOnly: true,
       secure: true,
+      sameSite: isProd ? 'strict' : 'none',
       maxAge: this.configService.get('JWT_ACCESS_TOKEN_EXP'),
     })
 
     res.cookie('refreshToken', tokenData.refreshToken, {
       httpOnly: true,
       secure: true,
+      sameSite: isProd ? 'strict' : 'none',
       maxAge: this.configService.get('JWT_REFRESH_TOKEN_EXP'),
     })
 
     return { message: 'login success' }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  authMe(@Req() req: Request) {
+    const user = req.user.userId
+
+    return { success: true, userId: user }
   }
 
   @Post('/refresh')
@@ -48,6 +60,7 @@ export class AuthController {
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
   ) {
+    const isProd = process.env.NODE_ENV === 'production'
     const refreshToken = req.cookies.refreshToken
     if (!refreshToken) throw new UnauthorizedException()
 
@@ -56,12 +69,14 @@ export class AuthController {
     res.cookie('accessToken', tokenData.accessToken, {
       httpOnly: true,
       secure: true,
+      sameSite: isProd ? 'strict' : 'none',
       maxAge: this.configService.get('JWT_ACCESS_TOKEN_EXP'),
     })
 
     res.cookie('refreshToken', tokenData.refreshToken, {
       httpOnly: true,
       secure: true,
+      sameSite: isProd ? 'strict' : 'none',
       maxAge: this.configService.get('JWT_REFRESH_TOKEN_EXP'),
     })
 
@@ -72,11 +87,21 @@ export class AuthController {
   @Post('/logout')
   async logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     const id = req.user.sub
-
+    const isProd = process.env.NODE_ENV === 'production'
     await this.authService.logout(id)
 
-    res.clearCookie('accessToken')
-    res.clearCookie('refreshToken')
+    res.clearCookie('accessToken', {
+      httpOnly: true,
+      secure: true,
+      sameSite: isProd ? 'strict' : 'none',
+      path: '/',
+    })
+    res.clearCookie('refreshToken', {
+      httpOnly: true,
+      secure: true,
+      sameSite: isProd ? 'strict' : 'none',
+      path: '/',
+    })
 
     return { message: 'logout success' }
   }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
     origin: ['http://localhost:3000', 'https://saebaldong.kr'],
+    credentials: true,
   })
 
   app.useGlobalPipes(
