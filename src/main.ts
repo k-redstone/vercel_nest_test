d12bfefb982c22c4533f817a9e382b4f7f1835e4
k-redstone <che0618@naver.com>
2025-05-18 19:36:16 +0900

    chore: dist & cors 해제

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://saebaldong.kr'],
+    origin: true,
     credentials: true,
   })
 
